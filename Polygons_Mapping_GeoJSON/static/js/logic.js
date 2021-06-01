95f098ed6f32251efa971aec9577ddb4baae1178
jennytran101 <79950817+jennytran101@users.noreply.github.com>
2021-06-01 18:11:06 -0500

    Updated Polygon GeoJSON map

--- Polygons_Mapping_GeoJSON/static/js/logic.js
+++ Polygons_Mapping_GeoJSON/static/js/logic.js
@@ -1,4 +1,4 @@
-// console.log("working");
+console.log("working");
 // let cityData = cities;
 
 // // Create the map object with center and zoom level.
@@ -12,23 +12,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/til
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy  © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  "Street": streets,
+  "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [streets]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -40,20 +40,12 @@ L.control.layers(baseMaps).addTo(map);
 // streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/jennytran101/Mapping_Earthquakes/master/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/jennytran101/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-    console.log(data);
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    color: "#ffffa1",
-    weight: 2,
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-      + feature.properties.dst + "</h3>");
-    }
-  })
-  .addTo(map);
+  L.geoJson(data).addTo(map);
 });
