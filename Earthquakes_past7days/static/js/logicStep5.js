b5f88a472d1d727ac7e0e210f321982f9b87eef0
jennytran101 <79950817+jennytran101@users.noreply.github.com>
2021-06-02 09:46:05 -0500

    Add a Legend to the Map

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -1,6 +1,23 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// // OLDER EXERCISE - Get data from cities.js
+// let cityData = cities;
+
+
+// // OLD EXERCISE - Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/200000,
+//     color: 'orange',
+//     weight: '4'
+//   })
+//   .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -30,30 +47,7 @@ let overlays = {
   Earthquakes: earthquakes
 };
 
-// Create a legend control object.
-let legend = L.control({
-  position: "bottomright"
-});
 
-// Then add all the details for the legend.
-legend.onAdd = function() {
-  let div = L.DomUtil.create("div", "info legend");
-  const magnitudes = [0, 1, 2, 3, 4, 5];
-  const colors = [
-    "#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"
-  ];
-
-
-
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
-// L.control.layers(baseMaps, overlays).addTo(map);
 
 
 
@@ -65,7 +59,8 @@ let map = L.map('mapid', {
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps,overlays).addTo(map);
+
 
 
 // Retrieve the earthquake GeoJSON data.
@@ -115,8 +110,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return magnitude * 4;
   }
 
-   // Creating a GeoJSON layer with the retrieved data.
-   L.geoJson(data, {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
     // We turn each feature into a circleMarker on the map.
     pointToLayer: function(feature, latlng) {
         console.log(data);
@@ -133,5 +128,36 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // Add the earthquake layer to our map
   earthquakes.addTo(map);
-});
+
+  // ADDING LEGEND TO THE MAP
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // Then add all the details for the legend.
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
   
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+  
+});  
